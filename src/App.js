00d15d40252e7131495b3449199fc76b9f98c1d4
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-10-31 21:20:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [skill, setSkill] = useState('');
+	const [mySkills, setMySkills] = useState([]);
+
+	function handleAddSkill(event) {
+		if (event) event.preventDefault();
+		setMySkills([...mySkills, skill]);
+		setSkill('');
+	}
+
+	function handleClearMySkills() {
+		setMySkills([]);
+	}
+
+	return (
+		<div className="container">
+			<div className="header">
+				<h1>Intro Cypress React Recife</h1>
+			</div>
+			<div className="content">
+				<div className="actions">
+					<input
+						name="skill"
+						type="text"
+						placeholder="Add skill"
+						value={skill}
+						onChange={e => setSkill(e.target.value)}
+					/>
+					<button onClick={handleAddSkill}>Add skill</button>
+					{mySkills && mySkills.length > 0 && (
+						<button onClick={handleClearMySkills}>Remove all</button>
+					)}
+				</div>
+				<div className="skill-list">
+					{mySkills && mySkills.length > 0 ? (
+						<ul>
+							{mySkills.map((skill, index) => (
+								<li key={index}>{skill}</li>
+							))}
+						</ul>
+					) : (
+						<p>Empty List</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
